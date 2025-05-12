29accce12afcbccc2f251628e01a11d436e50ccf
wahhabkhan <mwahab.1611@gmail.com>
2025-05-12 18:23:52 +0500

    Time Complexity 0

---
+++ 0001 - Two Sum.py
@@ -0,0 +1,10 @@
+def twoSum(nums, target):
+    num_map = {}  
+    
+    for index, num in enumerate(nums):
+        complement = target - num  
+        
+        if complement in num_map:
+            return [num_map[complement], index]
+        
+        num_map[num] = index  
