940d5b6fcaf5953c11333567f855d4f45f6fb3be
wahhabkhan <mwahab.1611@gmail.com>
2025-05-17 20:10:18 +0500

    Time 19ms (43.32%), Space 14.71MB (11.47%)

---
+++ 0125. Valid Palindrome.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def isPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        cleaned = [c.lower() for c in s if c.isalnum()]
+        return cleaned == cleaned[::-1]
+
+  
