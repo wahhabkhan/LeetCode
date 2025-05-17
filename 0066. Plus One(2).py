385d4d149af6505d18209ab37bebaadc3fb6eeb3
wahhabkhan <mwahab.1611@gmail.com>
2025-05-17 20:31:22 +0500

    Time 0ms (100%), Space 12.40MB (79.50%)

---
+++ 0066. Plus One(2).py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def plusOne(self, digits):
+        """
+        :type digits: List[int]
+        :rtype: List[int]
+        """
+        num = int(''.join(map(str, digits)))  
+        num += 1 
