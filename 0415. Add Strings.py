fdb45bdf6a8e702f17d3004a930f32b1d8a8bb8c
wahhabkhan <mwahab.1611@gmail.com>
2025-05-27 20:53:30 +0500

    Time 7ms (45.44%), Space 12.65MB (16.45%)

---
+++ 0415. Add Strings.py
@@ -0,0 +1,25 @@
+class Solution(object):
+    def addStrings(self, num1, num2):
+        """
+        :type num1: str
+        :type num2: str
+        :rtype: str
+        """
+        i = len(num1) - 1
+        j = len(num2) - 1
+        carry = 0
+        result = []
+
+        while i >= 0 or j >= 0 or carry:
+           digit1 = ord(num1[i]) - ord('0') if i >= 0 else 0
+           digit2 = ord(num2[j]) - ord('0') if j >= 0 else 0
+
+           total = digit1 + digit2 + carry
+           carry = total // 10
+           result.append(str(total % 10))
+
+           i -= 1
+           j -= 1
+
+        return ''.join(reversed(result))
+        
