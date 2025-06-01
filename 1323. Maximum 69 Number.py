ff291c5a8b52394bf99c8750644341941574a713
wahhabkhan <mwahab.1611@gmail.com>
2025-06-01 12:08:42 +0500

    Time 0ms (100%), Space 12.33MB (81.75%)

---
+++ 1323. Maximum 69 Number.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def maximum69Number (self, num):
+        """
+        :type num: int
+        :rtype: int
+        """
+        return int(str(num).replace('6','9',1))
+        
