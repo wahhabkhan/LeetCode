d173b4c07c4e3513985384cb75085b8455a5ea98
wahhabkhan <mwahab.1611@gmail.com>
2025-06-03 17:40:32 +0500

    Time 0ms (100%), Space 13.48MB (31.42%)

---
+++ 0704. Binary Search.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def search(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        n = len(nums)
+        left, right = 0, n - 1
+        while left <= right:
+            mid = (left + right) / 2
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] < target:
+                left = mid + 1
+            else:
+                right = mid - 1
+
+        return -1
+
+
+        
