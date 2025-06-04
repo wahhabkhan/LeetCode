ac4eeb26ea64255138fa3f0b499a10949ce94941
wahhabkhan <mwahab.1611@gmail.com>
2025-06-04 21:28:20 +0500

    Time 3ms (61.73%), Space 12.71MB (10.70%)

---
+++ 2248. Intersection of Multiple Arrays.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def intersection(self, nums):
+        """
+        :type nums: List[List[int]]
+        :rtype: List[int]
+        """
+        result = set(nums[0])
+
+        for arr in nums[1:]:
+            result &= set(arr)
+
+        return sorted(result)
+        
