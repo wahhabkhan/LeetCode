eefb45cea812b197ae81850a6d671417884303cf
wahhabkhan <mwahab.1611@gmail.com>
2025-07-05 22:27:21 +0500

    Time 0ms (100%), Space 12.50MB (9.37%)

---
+++ 1991. Find the Middle Index in Array.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def findMiddleIndex(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        total = sum(nums)
+        left_sum = 0
+
+        for i in range(len(nums)):
+            if left_sum == total - left_sum - nums[i]:
+                return i
+            left_sum += nums[i]
+
+        return -1
+        
