c54ca46c8a4140c8eede921a119fe53890c0155f
wahhabkhan <mwahab.1611@gmail.com>
2025-07-25 17:08:58 +0500

    Time 0ms (100%), Space 12.34MB (81.23%)

---
+++ 0119. Pascal's Triangle II.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def getRow(self, rowIndex):
+        """
+        :type rowIndex: int
+        :rtype: List[int]
+        """
+        row = [0] * (rowIndex+1)
+        row[0] = 1
+        for i in range(1,rowIndex+1):
+            for j in range(i,0,-1):
+                row[j] += row[j-1]
+
+        return row
+        
