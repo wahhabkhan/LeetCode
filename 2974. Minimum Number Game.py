3118f9eff1c7e9a2026ca4cc637b43d993ab93d1
wahhabkhan <mwahab.1611@gmail.com>
2025-08-07 23:09:39 +0500

    Time 0ms (100%), Space 12.44MB (51.92%)

---
+++ 2974. Minimum Number Game.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def numberGame(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        nums.sort();
+        output=[];
+        for i in range(0,len(nums),2):
+            output.append(nums[i+1]);
+            output.append(nums[i]);
+        return output;
