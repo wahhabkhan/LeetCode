5f1378cc3ad6c78fb31214eb152de06a3ee9d6cc
wahhabkhan <mwahab.1611@gmail.com>
2025-08-25 17:13:52 +0500

    Time 0ms (100%), Space 12.27MB (98.85%)

---
+++ 0342. Power of Four.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def isPowerOfFour(self, n):
+        """
+        :type n: int
+        :rtype: bool
+        """
+        return n>0 and (n & n-1) == 0 and (n-1)%3 == 0
+        
